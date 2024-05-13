9435b05c48e76dca73db44b231d89c8b662fe48e
Daran Dachte <darandachte@gmail.com>
2024-05-13 15:59:28 +0200

    transferring props to components

--- components/CategoriesList.vue
+++ components/CategoriesList.vue
@@ -1,27 +1,41 @@
 <template>
-  <div>
-    <h1>CategoriesList</h1>
-    <div class="flex flex-col items-center w-[10rem]">
-      <h1 class="mb-5">Categories</h1>
-      <ul
-        class="max-w-md space-y-1 text-gray-500 list-disc list-inside dark:text-gray-400"
-      >
-        <li>one</li>
-        <li>two</li>
-        <li>three</li>
-        <li>one</li>
-        <li>two</li>
-        <li>three</li>
-        <li>one</li>
-        <li>two</li>
-        <li>three</li>
-        <li>one</li>
-        <li>two</li>
-        <li>three</li>
-        <li>one</li>
-        <li>two</li>
-        <li>three</li>
-      </ul>
+  <template>
+    <div>
+      <div class="flex flex-col items-center w-[10rem]">
+        <h1 class="mb-5">Categories</h1>
+        <ul
+          class="max-w-md space-y-1 text-gray-500 list-disc list-inside dark:text-gray-400"
+        >
+          <li
+            class="hover:cursor-pointer"
+            v-for="category in categories"
+            :key="category"
+            @click="onCategoryClick(category)"
+            :class="{ 'font-bold': category === activeCategory }"
+          >
+            {{ category }}
+          </li>
+        </ul>
+      </div>
     </div>
-  </div>
+  </template>
 </template>
+
+<script>
+export default {
+  props: {
+    categories: {
+      type: Array,
+      required: true, // categories обязательный пропс
+    },
+    activeCategory: {
+      type: String,
+      required: true, // activeCategory обязательный пропс
+    },
+    onCategoryClick: {
+      type: Function,
+      required: true, // onCategoryClick обязательный пропс
+    },
+  },
+};
+</script>
