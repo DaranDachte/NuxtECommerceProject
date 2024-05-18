1065344770d587d4b27227611539567702139900
Daran Dachte <darandachte@gmail.com>
2024-05-18 18:36:42 +0200

    detail card design

--- pages/products/[id].vue
+++ pages/products/[id].vue
@@ -1,94 +1,120 @@
 <template>
-  <div
-    class="max-w-xl mt-10 p-6 mx-auto bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700"
-  >
-    <div class="max-w-[50rem] rounded-xl">
-      <UCarousel
-        v-slot="{ item }"
-        :items="items"
-        :ui="{
-          item: 'basis-full',
-          container: 'rounded-lg',
-        }"
-        :prev-button="{
-          color: 'gray',
-          icon: 'i-heroicons-arrow-left-20-solid',
-          class: '-left-12',
-        }"
-        :next-button="{
-          color: 'gray',
-          icon: 'i-heroicons-arrow-right-20-solid',
-          class: '-right-12',
-        }"
-        arrows
-        class="w-64 mx-auto"
-      >
-        <img :src="item" class="w-full" draggable="false" />
-      </UCarousel>
-    </div>
-
-    <h5
-      class="mb-2 text-2xl font-bold mt-4 tracking-tight text-gray-900 dark:text-white"
+  <div class="grid grid-cols-4 gap-x-2 px-4 w-full h-[80vh]">
+    <div
+      class="col-span-1 p-4 mt-10 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700 flex flex-col justify-between"
     >
-      <Icon name="flat-color-icons:about" /> {{ product?.title }}
-    </h5>
-    <div class="mb-3 text-xl font-bold text-gray-700 dark:text-gray-400">
-      <p>Product ID: {{ productId }}</p>
-      <p>Category: {{ product?.category }}</p>
-      <p>Product Name: {{ product?.brand }}</p>
-
-      <p>Description: {{ product?.description }}</p>
-      <p>In stock: {{ product?.stock }}</p>
-      <p>Price: {{ product?.price }} Euro</p>
-      <p>
-        <Icon name="line-md:star-twotone" color="#60CBD2" />Rating:
-        {{ product?.rating }}
+      <p
+        class="mb-3 text-xl font-bold text-gray-700 dark:text-gray-400 leading-10"
+      >
+        Our store offers thousands of products at an affordable price. You can
+        find from us everything according to your taste. If you have any
+        suggestions - how we can improve our work, or you have not found the
+        products you need, or you just want to communicate with us - please
+        follow this link and send us a message. We will read it carefully and
+        contact you if necessary.
       </p>
+      <NuxtLink
+        to="/contact"
+        class="flex items-center justify-center rounded-md bg-slate-900 px-5 py-2.5 text-center text-sm font-medium text-white hover:bg-gray-700 focus:outline-none focus:ring-4 focus:ring-blue-300"
+      >
+        <Icon name="i-ic:baseline-contact-mail" /> &nbsp; Go to contact page
+      </NuxtLink>
     </div>
+    <div
+      class="col-span-2 p-4 mt-10 mx-auto bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700 flex flex-col justify-between"
+    >
+      <div>
+        <UCarousel
+          v-slot="{ item }"
+          :items="items"
+          :ui="{
+            item: 'basis-full',
+            container: 'rounded-lg',
+          }"
+          :prev-button="{
+            color: 'gray',
+            icon: 'i-heroicons-arrow-left-20-solid',
+            class: '-left-12',
+          }"
+          :next-button="{
+            color: 'gray',
+            icon: 'i-heroicons-arrow-right-20-solid',
+            class: '-right-12',
+          }"
+          arrows
+          class="w-64 mx-auto"
+        >
+          <img :src="item" class="w-full" draggable="false" />
+        </UCarousel>
+      </div>
+      <div class="mb-3 text-xl font-bold text-gray-700 dark:text-gray-400">
+        <p class="flex items-center">
+          <Icon name="flat-color-icons:about" /> &nbsp;{{ product?.title }}
+        </p>
+        <p class="flex items-center">
+          <Icon name="i-fluent-emoji-high-contrast:id-button" color="#4FB563" />
+          &nbsp;Product ID: {{ productId }}
+        </p>
+        <p class="flex items-center">
+          <Icon
+            name="i-fluent-emoji-high-contrast:green-square"
+            color=""
+          />&nbsp;Category: {{ capitalizeFirstLetter(product?.category ?? "") }}
+        </p>
+        <p class="flex items-center">
+          <Icon
+            name="i-fluent-emoji-high-contrast:clipboard"
+            color="#4FB5A9"
+          />&nbsp;Product Name:
+          {{ product?.brand }}
+        </p>
 
-    <div class="flex justify-between">
+        <p class="flex items-center">
+          <Icon name="i-heroicons-solid:home" color="#604FB5" />&nbsp;In stock:
+          {{ product?.stock }}
+        </p>
+        <p class="flex items-center">
+          <Icon name="i-cryptocurrency-color:eur" />&nbsp;Price:
+          {{ product?.price }} Euro
+        </p>
+        <p class="flex items-center">
+          <Icon name="i-subway:star" color="#FFD700" />&nbsp;Rating:
+          {{ product?.rating }}
+        </p>
+        <p>Description: {{ product?.description }}</p>
+      </div>
+
+      <div class="flex justify-between">
+        <NuxtLink
+          to="/"
+          class="flex items-center justify-center rounded-md bg-slate-900 px-5 py-2.5 text-center text-sm font-medium text-white hover:bg-gray-700 focus:outline-none focus:ring-4 focus:ring-blue-300"
+        >
+          <Icon name="i-ic:baseline-keyboard-arrow-left" /> &nbsp; Back to
+          Catalog
+        </NuxtLink>
+        <NuxtLink
+          href="#"
+          class="flex items-center justify-center rounded-md bg-slate-900 px-5 py-2.5 text-center text-sm font-medium text-white hover:bg-gray-700 focus:outline-none focus:ring-4 focus:ring-blue-300"
+        >
+          <Icon name="i-ic:baseline-local-grocery-store" /> &nbsp; Add to cart
+        </NuxtLink>
+      </div>
+    </div>
+    <div
+      class="col-span-1 p-4 max-w-xl mt-10 mx-auto bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700 flex flex-col justify-between"
+    >
+      <p
+        class="mb-3 text-xl font-bold text-gray-700 dark:text-gray-400 leading-10"
+      >
+        You can add your favorite item to your wish list and come back to it
+        later. Maybe the price becomes more affordable, maybe you want to see
+        the details of the item again.
+      </p>
       <NuxtLink
         to="/"
         class="flex items-center justify-center rounded-md bg-slate-900 px-5 py-2.5 text-center text-sm font-medium text-white hover:bg-gray-700 focus:outline-none focus:ring-4 focus:ring-blue-300"
       >
-        <!-- Иконка -->
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          fill="none"
-          viewBox="0 0 24 24"
-          stroke-width="1.5"
-          stroke="currentColor"
-          class="w-6 h-6"
-        >
-          <path
-            stroke-linecap="round"
-            stroke-linejoin="round"
-            d="M15.75 9V5.25A2.25 2.25 0 0 0 13.5 3h-6a2.25 2.25 0 0 0-2.25 2.25v13.5A2.25 2.25 0 0 0 7.5 21h6a2.25 2.25 0 0 0 2.25-2.25V15M12 9l-3 3m0 0 3 3m-3-3h12.75"
-          />
-        </svg>
-
-        Back to Catalog
-      </NuxtLink>
-      <NuxtLink
-        href="#"
-        class="flex items-center justify-center rounded-md bg-slate-900 px-5 py-2.5 text-center text-sm font-medium text-white hover:bg-gray-700 focus:outline-none focus:ring-4 focus:ring-blue-300"
-      >
-        <!-- Иконка -->
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          class="mr-2 h-6 w-6"
-          fill="none"
-          viewBox="0 0 24 24"
-          stroke="currentColor"
-          stroke-width="2"
-        >
-          <path
-            stroke-linecap="round"
-            stroke-linejoin="round"
-            d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z"
-          />
-        </svg>
-        Add to cart
+        <Icon name="i-heroicons-solid:heart" /> &nbsp; Go to wish list
       </NuxtLink>
     </div>
   </div>
@@ -102,6 +128,10 @@ const categoriesStore = useCategoriesStore();
 
 const route = useRoute();
 const productId = route.params.id;
+
+const capitalizeFirstLetter = (str: string) => {
+  return str.charAt(0).toUpperCase() + str.slice(1);
+};
 
 interface Product {
   id: number;
