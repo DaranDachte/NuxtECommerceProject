ed0cd520b819193b5cd6c64dce7d6c32d33dc259
Daran Dachte <darandachte@gmail.com>
2024-05-30 21:17:38 +0200

    id details card design

--- pages/products/[id].vue
+++ pages/products/[id].vue
@@ -1,7 +1,8 @@
 <template>
-  <div class="grid grid-cols-4 mt-5 gap-x-2 px-4 w-full">
+  <div class="flex mt-1 gap-4 px-4 w-full">
+    <!--   левая  колонка начало -->
     <div
-      class="col-span-1 p-4 border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700 flex flex-col justify-between"
+      class="basis-[15%] p-4 border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700 flex flex-col justify-between"
     >
       <p
         class="mb-3 text-xl font-bold text-gray-700 dark:text-gray-400 leading-10"
@@ -20,8 +21,12 @@
         <Icon name="i-ic:baseline-contact-mail" /> &nbsp; Go to contact page
       </NuxtLink>
     </div>
+
+    <!-- левая колонка конец -->
+
+    <!-- центральная колонка начало -->
     <div
-      class="col-span-2 flex-grow p-4 mx-auto border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700 flex flex-col justify-between"
+      class="basis-[70%] p-4 mx-auto border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700 flex flex-col"
     >
       <div>
         <UCarousel
@@ -44,85 +49,123 @@
           arrows
           class="w-[25rem] mx-auto"
         >
-          <img :src="item" class="max-h-[25rem]" draggable="false" />
+          <img :src="item" class="max-h-[20rem]" draggable="false" />
         </UCarousel>
       </div>
-      <div class="mb-3 text-xl font-bold text-gray-700 dark:text-gray-400">
-        <p class="flex items-center">
-          <Icon name="flat-color-icons:about" /> &nbsp;{{ product?.title }}
-        </p>
-        <p class="flex items-center">
-          <Icon name="i-fluent-emoji-high-contrast:id-button" color="#4FB563" />
-          &nbsp;Product ID: {{ productId }}
-        </p>
-        <p class="flex items-center">
-          <Icon
-            name="i-fluent-emoji-high-contrast:green-square"
-            color=""
-          />&nbsp;Category: {{ capitalizeFirstLetter(product?.category ?? "") }}
-        </p>
-        <p class="flex items-center">
-          <Icon
-            name="i-fluent-emoji-high-contrast:clipboard"
-            color="#4FB5A9"
-          />&nbsp;Product Name:
-          {{ product?.brand }}
-        </p>
-        <p class="flex items-center">
-          <Icon
-            name="i-ic:baseline-assignment-turned-in"
-            color="#9414AD"
-          />&nbsp; Stock status:
-          {{ product?.availabilityStatus }}
-        </p>
-        <p class="flex items-center">
-          <Icon name="i-heroicons-solid:home" color="#604FB5" />&nbsp;In stock
-          quantity:
-          {{ product?.stock }}
-        </p>
-        <p class="flex items-center">
-          <Icon
-            name="i-ic:baseline-app-registration"
-            color="#14AD8F"
-          />&nbsp;Minimum order quantity:
-          {{ product?.minimumOrderQuantity }}
-        </p>
-        <p class="flex items-center">
-          <Icon name="i-ic:baseline-info" color="#14AD8F" />&nbsp;Shipping
-          Information:
-          {{ product?.shippingInformation }}
-        </p>
-        <p class="flex items-center">
-          <Icon name="i-cryptocurrency-color:eur" />&nbsp;Price:
-          {{ product?.price }} Euro
-        </p>
-        <p class="flex items-center">
-          <Icon name="i-subway:star" color="#FFD700" />&nbsp;Rating:
-          {{ product?.rating }}
-        </p>
-        <p class="flex items-center">
-          <Icon name="i-grommet-icons:task" color="#1714AD" /> &nbsp; Size: W:
-          {{ product?.dimensions.width }} x H:
-          {{ product?.dimensions.height }} x D:
-          {{ product?.dimensions.depth }}
-        </p>
-        <p class="flex items-center">
-          <Icon name="i-ic:twotone-line-weight" color="#849A96" /> &nbsp;
-          Weight:
-          {{ product?.weight }}
-        </p>
-        <p class="flex items-center">
-          <Icon
-            name="i-heroicons-solid:arrow-left-on-rectangle"
-            color="#1473AD"
-          />
-          &nbsp; Return policy: {{ product?.returnPolicy }}
-        </p>
 
-        <p>Description: {{ product?.description }}</p>
+      <div
+        class="flex flex-col shadow-2xl text-2xl p-5 align-center justify-between border border-solid border-gray-200 bg-white dark:bg-gray-800 rounded-xl font-bold text-gray-700 dark:text-gray-400"
+      >
+        <div v-show="!expanded">
+          <p class="flex items-center">
+            <Icon name="flat-color-icons:about" /> &nbsp;{{ product?.title }}
+          </p>
+          <p class="flex items-center">
+            <Icon
+              name="i-fluent-emoji-high-contrast:id-button"
+              color="#4FB563"
+            />
+            &nbsp;Product ID: {{ productId }}
+          </p>
+          <p class="flex items-center">
+            <Icon
+              name="i-fluent-emoji-high-contrast:green-square"
+              color=""
+            />&nbsp;Category:
+            {{ capitalizeFirstLetter(product?.category ?? "") }}
+          </p>
+          <p class="flex items-center">
+            <Icon
+              name="i-fluent-emoji-high-contrast:clipboard"
+              color="#4FB5A9"
+            />&nbsp;Product Name:
+            {{ product?.brand }}
+          </p>
+        </div>
+        <div v-show="expanded">
+          <p class="flex items-center">
+            <Icon name="flat-color-icons:about" /> &nbsp;{{ product?.title }}
+          </p>
+          <p class="flex items-center">
+            <Icon
+              name="i-fluent-emoji-high-contrast:id-button"
+              color="#4FB563"
+            />
+            &nbsp;Product ID: {{ productId }}
+          </p>
+          <p class="flex items-center">
+            <Icon
+              name="i-fluent-emoji-high-contrast:green-square"
+              color=""
+            />&nbsp;Category:
+            {{ capitalizeFirstLetter(product?.category ?? "") }}
+          </p>
+          <p class="flex items-center">
+            <Icon
+              name="i-fluent-emoji-high-contrast:clipboard"
+              color="#4FB5A9"
+            />&nbsp;Product Name:
+            {{ product?.brand }}
+          </p>
+          <p class="flex items-center">
+            <Icon
+              name="i-ic:baseline-assignment-turned-in"
+              color="#9414AD"
+            />&nbsp; Stock status:
+            {{ product?.availabilityStatus }}
+          </p>
+          <p class="flex items-center">
+            <Icon name="i-heroicons-solid:home" color="#604FB5" />&nbsp;In stock
+            quantity:
+            {{ product?.stock }}
+          </p>
+          <p class="flex items-center">
+            <Icon
+              name="i-ic:baseline-app-registration"
+              color="#14AD8F"
+            />&nbsp;Minimum order quantity:
+            {{ product?.minimumOrderQuantity }}
+          </p>
+          <p class="flex items-center">
+            <Icon name="i-ic:baseline-info" color="#14AD8F" />&nbsp;Shipping
+            Information:
+            {{ product?.shippingInformation }}
+          </p>
+          <p class="flex items-center">
+            <Icon name="i-cryptocurrency-color:eur" />&nbsp;Price:
+            {{ product?.price }} Euro
+          </p>
+          <p class="flex items-center">
+            <Icon name="i-subway:star" color="#FFD700" />&nbsp;Rating:
+            {{ product?.rating }}
+          </p>
+          <p class="flex items-center">
+            <Icon name="i-grommet-icons:task" color="#1714AD" /> &nbsp; Size: W:
+            {{ product?.dimensions.width }} x H:
+            {{ product?.dimensions.height }} x D:
+            {{ product?.dimensions.depth }}
+          </p>
+          <p class="flex items-center">
+            <Icon name="i-ic:twotone-line-weight" color="#849A96" /> &nbsp;
+            Weight:
+            {{ product?.weight }}
+          </p>
+          <p class="flex items-center">
+            <Icon
+              name="i-heroicons-solid:arrow-left-on-rectangle"
+              color="#1473AD"
+            />
+            &nbsp; Return policy: {{ product?.returnPolicy }}
+          </p>
+
+          <p>Description: {{ product?.description }}</p>
+        </div>
+        <button @click="toggleExpanded" class="text-blue-500 mt-4">
+          {{ expanded ? "View less" : "View more" }}
+        </button>
       </div>
 
-      <div class="flex justify-between">
+      <div class="flex justify-between mt-5">
         <NuxtLink
           to="/"
           class="flex items-center justify-center rounded-md bg-slate-900 px-5 py-2.5 text-center text-sm font-medium text-white hover:bg-gray-700 focus:outline-none focus:ring-4 focus:ring-blue-300"
@@ -138,9 +181,11 @@
         </NuxtLink>
       </div>
     </div>
+    <!-- центральная колонка конец -->
 
+    <!-- правая колонка начало -->
     <div
-      class="col-span-1 flex-grow p-4 max-w-xl mx-auto border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700 flex flex-col justify-between"
+      class="basis-[15%] p-2 mx-auto border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700 flex flex-col justify-between"
     >
       <div>
         <p
@@ -150,17 +195,17 @@
           later. Maybe the price becomes more affordable, maybe you want to see
           the details of the item again.
         </p>
+
         <div class="flex justify-between">
           <button
             @click="wishListStore.addToWishList(product)"
-            class="flex items-center justify-center rounded-md bg-slate-900 px-5 py-2.5 text-center text-sm font-medium text-white hover:bg-gray-700 focus:outline-none focus:ring-4 focus:ring-blue-300"
+            class="flex items-center justify-center rounded-md bg-slate-900 px-2 py-1 text-center text-[0.9rem] font-medium text-white hover:bg-gray-700 focus:outline-none focus:ring-4 focus:ring-blue-300"
           >
             <Icon name="i-heroicons-solid:heart" /> &nbsp; Add to wish list
           </button>
-
           <NuxtLink
             to="/wishlist"
-            class="flex items-center justify-center rounded-md bg-slate-900 px-5 py-2.5 text-center text-sm font-medium text-white hover:bg-gray-700 focus:outline-none focus:ring-4 focus:ring-blue-300"
+            class="flex items-center justify-center rounded-md bg-slate-900 px-2 py-1 text-center text-[0.9rem] font-medium text-white hover:bg-gray-700 focus:outline-none focus:ring-4 focus:ring-blue-300"
           >
             <Icon name="i-heroicons-solid:heart" /> &nbsp; Go to wish list
           </NuxtLink>
@@ -170,14 +215,14 @@
         <ReviewsList v-if="product?.id" :productId="product.id" />
       </div>
     </div>
+    <!--   правая колонка конец -->
   </div>
-
   <div class="relative">
     <UAlert
       v-if="wishListStore.showAlert"
       description="You have already added this item to your wish list."
       title="Heads up!"
-      class="fixed top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 w-[25rem] h-[5rem] bg-gradient-to-r from-red-200 to-red-600"
+      class="fixed top-[25%] left-1/2 transform -translate-x-1/2 -translate-y-1/2 w-[25rem] h-[5rem] bg-gradient-to-r from-red-200 to-red-600"
     />
   </div>
 </template>
@@ -191,6 +236,11 @@ const categoriesStore = useCategoriesStore();
 const wishListStore = useWishListStore();
 const route = useRoute();
 const productId = route.params.id;
+const expanded = ref(false);
+const toggleExpanded = () => {
+  expanded.value = !expanded.value;
+};
+
 const capitalizeFirstLetter = (str: string) => {
   return str.charAt(0).toUpperCase() + str.slice(1);
 };
